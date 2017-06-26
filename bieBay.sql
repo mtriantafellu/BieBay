ddb1f4fa261506b86f382f76a5e46ec696c6ba75
Matthew <mtriantafellu@users.noreply.github.com>
2017-06-26 12:06:44 -0400

    with video file working

--- bieBay.sql
+++ bieBay.sql
@@ -1,25 +1,54 @@
-#use biebay
+use biebay;
 
-# CREATE TABLE `products` (
-# `id` INT AUTO_INCREMENT,
-# `item_id` varchar(128),
-# `product_name` varchar(128),
-# `department_name` varchar(128),
-# `price` int,
-# `stock_quantity` int,
-# `autographed` boolean,
-# PRIMARY KEY(`id`));
+#CREATE TABLE `products` (
+#`id` INT AUTO_INCREMENT,
+#`item_id` varchar(128),
+#`product_name` varchar(128),
+#`department_name` varchar(128),
+#`price` int,
+#`stock_quantity` int,
+#`autographed` boolean,
+#PRIMARY KEY(`id`));
 
 DESCRIBE `products`;
 
-# INSERT INTO `products`
-# (`item_id`) VALUES ('Toothpaste');
+INSERT INTO `products` (`item_id`) VALUES ('Hairspray');
+INSERT INTO `products` (`item_id`) VALUES ('Pants');
+INSERT INTO `products` (`item_id`) VALUES ('Toothpaste');
+INSERT INTO `products` (`item_id`) VALUES ('Sunglasses');
+INSERT INTO `products` (`item_id`) VALUES ('Sneakers');
 
-UPDATE `products` SET `stock_quantity` = '134' WHERE id = '1';
-UPDATE `products` SET `stock_quantity` = '182' WHERE id = '2';
-UPDATE `products` SET `stock_quantity` = '42' WHERE id = '3';
-UPDATE `products` SET `stock_quantity` = '98' WHERE id = '4';
-UPDATE `products` SET `stock_quantity` = '253' WHERE id = '5';
+UPDATE `products` SET `product_name` = 'BiebSpray' WHERE id = '1';
+UPDATE `products` SET `product_name` = 'BaggyPants' WHERE id = '2';
+UPDATE `products` SET `product_name` = 'BiebPaste' WHERE id = '3';
+UPDATE `products` SET `product_name` = 'TooCool' WHERE id = '4';
+UPDATE `products` SET `product_name` = 'Biekers' WHERE id = '5';
+
+UPDATE `products` SET `department_name` = 'grocery' WHERE id = '1';
+UPDATE `products` SET `department_name` = 'apparel' WHERE id = '2';
+UPDATE `products` SET `department_name` = 'grocery' WHERE id = '3';
+UPDATE `products` SET `department_name` = 'accessories' WHERE id = '4';
+UPDATE `products` SET `department_name` = 'footwear' WHERE id = '5';
+
+UPDATE `products` SET `price` = '5' WHERE id = '1';
+UPDATE `products` SET `price` = '25' WHERE id = '2';
+UPDATE `products` SET `price` = '10' WHERE id = '3';
+UPDATE `products` SET `price` = '90' WHERE id = '4';
+UPDATE `products` SET `price` = '120' WHERE id = '5';
+
+UPDATE `products` SET `stock_quantity` = '125' WHERE id = '1';
+UPDATE `products` SET `stock_quantity` = '125' WHERE id = '2';
+UPDATE `products` SET `stock_quantity` = '110' WHERE id = '3';
+UPDATE `products` SET `stock_quantity` = '190' WHERE id = '4';
+UPDATE `products` SET `stock_quantity` = '120' WHERE id = '5';
+
+ #id, item_id, product_name, department_name, stock_quantity, autographed
+
+#UPDATE `products` SET `autographed` = '0' WHERE id = '1';
+#UPDATE `products` SET `autographed` = '0' WHERE id = '2';
+#UPDATE `products` SET `autographed` = '0' WHERE id = '3';
+#UPDATE `products` SET `autographed` = '1' WHERE id = '4';
+#UPDATE `products` SET `autographed` = '0' WHERE id = '5';
 
 SELECT * FROM `products`;
  
